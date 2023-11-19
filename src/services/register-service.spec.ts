3016f75d366e490897e7a79a8120175ec2cac3cf
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-19 13:28:01 -0300

    test: users can register

--- src/services/register-service.spec.ts
+++ src/services/register-service.spec.ts
@@ -7,6 +7,19 @@ import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 // Test Unit
 
 describe("Register service", () => {
+  it("should be able to register", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(usersRepository);
+
+    const { user } = await registerService.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
   it("should hash user password upon registration", async () => {
     const usersRepository = new InMemoryUsersRepository();
     const registerService = new RegisterService(usersRepository);
