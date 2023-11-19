aeca0d65831c40c1fe85d89573e591cb8be6ebce
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-19 16:02:06 -0300

    feat: user should be able to authenticate

---
+++ src/services/authenticate-service.spec.ts
@@ -0,0 +1,25 @@
+import { expect, describe, it } from "vitest";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { AuthenticateService } from "./authenticate-service";
+import { hash } from "bcryptjs";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+
+describe("Authenticate service", () => {
+  it("should be able to authenticate", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateService(usersRepository);
+
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    const { user } = await sut.execute({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+});
