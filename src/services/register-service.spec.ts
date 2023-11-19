be084f283050b2cda3fc134f6c319bf79747ba34
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-19 13:26:29 -0300

    test: users are unable to register with same email

--- src/services/register-service.spec.ts
+++ src/services/register-service.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from "vitest";
 import { RegisterService } from "./register-service";
 import { compare } from "bcryptjs";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 // Test Unit
 
@@ -22,5 +23,26 @@ describe("Register service", () => {
     );
 
     expect(isPasswordCorrectlyHashed).toBeTruthy();
+  });
+
+  it("should not be able to register with same email twice", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(usersRepository);
+
+    const email = "johndoe@example.com";
+
+    await registerService.execute({
+      name: "John Doe",
+      email,
+      password: "123456",
+    });
+
+    expect(async () => {
+      await registerService.execute({
+        name: "John Doe",
+        email,
+        password: "123456",
+      });
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });
