70663fa33b1b0eddf68eba6a32d30de38b968b42
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-19 16:04:10 -0300

    test: user should not be able to authenticate with wrong password

--- src/services/authenticate-service.spec.ts
+++ src/services/authenticate-service.spec.ts
@@ -35,4 +35,23 @@ describe("Authenticate service", () => {
         }),
     ).rejects.toBeInstanceOf(InvalidCredentialsError);
   });
+
+  it("should not be able to authenticate with wrong password", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const sut = new AuthenticateService(usersRepository);
+
+    await usersRepository.create({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password_hash: await hash("123456", 6),
+    });
+
+    expect(
+      async () =>
+        await sut.execute({
+          email: "johndoe@example.com",
+          password: "123123",
+        }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
 });
