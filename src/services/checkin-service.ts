e717699b8f72415d2039fa59563d7cafc9a18f7a
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-20 16:15:01 -0300

    feat: added `gymsRepository` in `checkInService`

--- src/services/checkin-service.ts
+++ src/services/checkin-service.ts
@@ -1,9 +1,13 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 interface CheckInResponse {
@@ -11,9 +15,20 @@ interface CheckInResponse {
 }
 
 export class CheckInService {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
 
   async execute({ userId, gymId }: CheckInRequest): Promise<CheckInResponse> {
+    const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    }
+
+    // calculate distance between user and gym
+
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
