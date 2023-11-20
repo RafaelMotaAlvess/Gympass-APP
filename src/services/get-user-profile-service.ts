eb2d65ea61d006d16084e563890a98bd239bcc3f
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-20 13:33:52 -0300

    feat: created `get-user-profile` service

---
+++ src/services/get-user-profile-service.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface GetUserProfileServiceRequest {
+  userId: string;
+}
+
+interface GetUserProfileServiceResponse {
+  user: User;
+}
+
+export class etUserProfileService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileServiceRequest): Promise<GetUserProfileServiceResponse> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ResourceNotFoundError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
