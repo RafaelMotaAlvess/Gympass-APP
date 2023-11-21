270f5717141e188006a8715e36f3e4b554e82b09
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-21 13:25:02 -0300

    feat: if "data" is not given a uuid, create a new one

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -17,7 +17,7 @@ export class InMemoryGymsRepository implements GymsRepository {
 
   async create(data: Prisma.GymCreateInput) {
     const gym = {
-      id: randomUUID(),
+      id: data.id ?? randomUUID(),
       title: data.title,
       description: data.description ?? null,
       phone: data.phone ?? null,
