55d91187570574e87492463e23934d6fab8e2ba9
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-21 15:59:47 -0300

    feat: added `page` parameter in `findManyByUserID`

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,6 +2,6 @@ import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
-  findManyByUserID(userId: string): Promise<CheckIn[]>;
+  findManyByUserID(userId: string, page: number): Promise<CheckIn[]>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
 }
