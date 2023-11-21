70e5fa9dd97ab0b2c7dc4e8dab71ad4cfaeeaeed
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-21 12:30:09 -0300

    feat: added distance validation for `checkin-service`

--- src/services/checkin-service.ts
+++ src/services/checkin-service.ts
@@ -2,6 +2,7 @@ import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { GymsRepository } from "@/repositories/gyms-repository";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { getDistanceBetweenCoordinates } from "@/utils/get-distance-between-coordinates";
 
 interface CheckInRequest {
   userId: string;
@@ -20,14 +21,31 @@ export class CheckInService {
     private gymsRepository: GymsRepository,
   ) {}
 
-  async execute({ userId, gymId }: CheckInRequest): Promise<CheckInResponse> {
+  async execute({
+    userId,
+    gymId,
+    userLatitude,
+    userLongitude,
+  }: CheckInRequest): Promise<CheckInResponse> {
     const gym = await this.gymsRepository.findById(gymId);
 
     if (!gym) {
       throw new ResourceNotFoundError();
     }
 
-    // calculate distance between user and gym
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    );
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error();
+    }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
