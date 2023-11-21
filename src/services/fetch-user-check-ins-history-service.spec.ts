ad2b04247bf2071780c56f6476704af065e4f0f1
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-11-21 16:03:22 -0300

    test: should be able to fetch paginated check-in history

--- src/services/fetch-user-check-ins-history-service.spec.ts
+++ src/services/fetch-user-check-ins-history-service.spec.ts
@@ -24,12 +24,33 @@ describe("Fetch User Check-in History Service", () => {
 
     const { checkIns } = await sut.execute({
       userId: "user-01",
+      page: 1,
     });
 
     expect(checkIns).toHaveLength(2);
     expect(checkIns).toEqual([
       expect.objectContaining({ gym_id: "gym-01" }),
       expect.objectContaining({ gym_id: "gym-02" }),
+    ]);
+  });
+
+  it("should be able to fetch paginated check-in history", async () => {
+    for (let index = 1; index <= 22; index++) {
+      await checkInRepository.create({
+        gym_id: `gym-${index}`,
+        user_id: "user-01",
+      });
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: "user-01",
+      page: 2,
+    });
+
+    expect(checkIns).toHaveLength(2);
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: "gym-21" }),
+      expect.objectContaining({ gym_id: "gym-22" }),
     ]);
   });
 });
