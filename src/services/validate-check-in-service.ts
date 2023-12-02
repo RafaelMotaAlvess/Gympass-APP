f3d6100a36157557ef0e725f814ed4dd448a52fb
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-02 20:18:00 -0300

    feat: check if the created check-in time is less than 20 minutes

--- src/services/validate-check-in-service.ts
+++ src/services/validate-check-in-service.ts
@@ -1,8 +1,8 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
-
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
-
+import dayjs from "dayjs";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 interface ValidateCheckInServiceRequest {
   checkInID: string;
 }
@@ -21,6 +21,15 @@ export class ValidateCheckInService {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes",
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
     }
 
     checkIn.validated_at = new Date();
