a16704854c4b567aeba030578536e5de1516a81d
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-14 15:44:24 -0300

    feat: added token for authenticated users

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -17,9 +17,21 @@ export async function authenticate(
   try {
     const authenticateService = makeAuthenticateService();
 
-    await authenticateService.execute({
+    const { user } = await authenticateService.execute({
       email,
       password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    );
+    return reply.status(200).send({
+      token,
     });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
@@ -28,6 +40,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
