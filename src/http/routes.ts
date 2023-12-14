888957c2ac7429d8449b814314f942b2f9d285ba
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-14 16:55:45 -0300

    feat: added `verifyJWT` middleware to `profile` controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,8 +2,13 @@ import { FastifyInstance } from "fastify";
 
 import { register } from "./controllers/register-controller";
 import { authenticate } from "./controllers/authenticate-controller";
+import { profile } from "./controllers/profile-controller";
+import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  /** Authenticated */
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
