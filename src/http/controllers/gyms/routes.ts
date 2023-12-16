81d9a4db875d78b818b8dd369c680c38888a4666
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-16 18:20:48 -0300

    chore: added controllers to gyms routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
+import { search } from "./search-controller";
+import { nearby } from "./nearby-controller";
+import { create } from "./create-controller";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+
+  app.post("/gyms", create);
 }
