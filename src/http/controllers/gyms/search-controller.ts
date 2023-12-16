cba1c271e15628f32235716a50cc7d38797f1d7c
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-16 18:20:06 -0300

    feat: created `search` gym controller

---
+++ src/http/controllers/gyms/search-controller.ts
@@ -0,0 +1,23 @@
+import { makeSearchGymService } from "@/services/factories/make-search-gym-service";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { query, page } = searchGymsQuerySchema.parse(request.body);
+
+  const searchGymsService = makeSearchGymService();
+
+  const { gyms } = await searchGymsService.execute({
+    query,
+    page,
+  });
+
+  return reply.status(201).send({
+    gyms,
+  });
+}
