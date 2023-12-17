77d2f656c42b003da3466a0588c52ec4f64195ce
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-17 13:25:55 -0300

    refactor: extraction of function for user creation and authentication

--- src/http/controllers/users/profile-controller.spec.ts
+++ src/http/controllers/users/profile-controller.spec.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "@/app";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
 
 describe("Profile (e2e)", () => {
   beforeAll(async () => {
@@ -12,18 +13,7 @@ describe("Profile (e2e)", () => {
   });
 
   it("should be able to get user profile", async () => {
-    await request(app.server).post("/users").send({
-      name: "Rafael Mota",
-      email: "rafael@example.com",
-      password: "123456",
-    });
-
-    const authResponse = await request(app.server).post("/sessions").send({
-      email: "rafael@example.com",
-      password: "123456",
-    });
-
-    const { token } = authResponse.body;
+    const { token } = await createAndAuthenticateUser(app);
 
     const profileResponse = await request(app.server)
       .get("/me")
