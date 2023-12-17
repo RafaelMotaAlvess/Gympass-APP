342c172050e060a329849e8a786460b7b0147b0a
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-17 13:33:29 -0300

    test: should be able to create a gym `E2E`

---
+++ src/http/controllers/gyms/create-controller.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+
+describe("Create Gym (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a gym", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const response = await request(app.server)
+      .post("/gyms")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "JavaScript Gym",
+        description: "Any description",
+        phone: "1199999999",
+        latitude: -27.0025779,
+        longitude: -48.6183042,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
