724ef9d62f03b47758d10bcebff1fd5881c3a49c
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-17 13:11:37 -0300

    feat: created `validate` check-ins controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,9 +1,11 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { FastifyInstance } from "fastify";
 import { create } from "./create-controller";
+import { validate } from "./validate-controller";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/gyms/:gymId/check-ins", create);
+  app.patch("/check-ins/:checkInId/validate", validate);
 }
