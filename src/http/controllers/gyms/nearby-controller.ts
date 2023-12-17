09f951e23b80567b9162b42e83b1a612895b7a72
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-17 14:13:39 -0300

    fix: converting latitude and longitude string into a number

--- src/http/controllers/gyms/nearby-controller.ts
+++ src/http/controllers/gyms/nearby-controller.ts
@@ -4,10 +4,10 @@ import { makeFetchNearbyGymsService } from "@/services/factories/make-fetch-near
 
 export async function nearby(request: FastifyRequest, reply: FastifyReply) {
   const nearbyGymsQuerySchema = z.object({
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 90;
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 180;
     }),
   });
@@ -21,7 +21,7 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
     userLongitude: longitude,
   });
 
-  return reply.status(201).send({
+  return reply.status(200).send({
     gyms,
   });
 }
