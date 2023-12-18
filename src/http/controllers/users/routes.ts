773b224a413f114fc3af5705f10eca4733e0b3b3
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-17 23:01:28 -0300

    feat: created `refresh` user controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,13 @@ import { register } from "./register-controller";
 import { authenticate } from "./authenticate-controller";
 import { profile } from "./profile-controller";
 import { verifyJWT } from "../../middlewares/verify-jwt";
+import { refresh } from "./refresh-controller";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   /** Authenticated */
   app.get("/me", { onRequest: [verifyJWT] }, profile);
