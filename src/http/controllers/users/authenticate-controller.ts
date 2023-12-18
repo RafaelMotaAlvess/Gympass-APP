bab08e6edd4164e2e197c4564da95ec0a07cca36
Rafael Mota Alves <86445038+RafaelMotaAlvess@users.noreply.github.com>
2023-12-17 22:50:53 -0300

    feat: setting `refreshToken` to cookies

--- src/http/controllers/users/authenticate-controller.ts
+++ src/http/controllers/users/authenticate-controller.ts
@@ -27,12 +27,31 @@ export async function authenticate(
       {
         sign: {
           sub: user.id,
+          expiresIn: "7d",
         },
       },
     );
-    return reply.status(200).send({
-      token,
-    });
+
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    );
+
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
