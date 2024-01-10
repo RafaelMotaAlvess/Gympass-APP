4b070c614aa668509ec99edb99056ee9298dfb05
RafaelMotaAlvess <leafardzn@gmail.com>
2024-01-10 20:34:33 -0300

    docs: created `search-gym-schema`

---
+++ src/docs/swagger/schemas/gyms/search-gym-schema.ts
@@ -0,0 +1,46 @@
+export const searchGymSchema = {
+  schema: {
+    description: 'Search gyms in database',
+    tags: ['gyms routes'],
+    summary: 'Search gyms',
+    response: {
+      200: {
+        description: 'Successful response',
+        type: 'object',
+        properties: {
+          gyms: {
+            type: 'array', 
+            items: {
+              type: 'object',
+              properties: {
+                id: {type: 'string'},
+                name: {type: 'string'},
+                latitude: {type: 'number'},
+                longitude: {type: 'number'},
+                created_at: {type: 'string', format: 'date-time'},
+              }
+            }
+          }
+        }
+        
+        
+      },
+      400: {
+        description: 'Validation error',
+        type: 'object',
+        properties: {
+          message: { type: 'string', example: "Validation error" },
+          issues: { type: 'string', example: "error" }
+        }
+      },
+      500: {
+        description: 'Default response',
+        type: 'object',
+        properties: {
+          message: { type: 'string', example: "Internal server error." }
+        }
+      }
+    },
+    
+  }
+}
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -4,12 +4,16 @@ import { FastifyInstance } from "fastify";
 import { search } from "./search-controller";
 import { nearby } from "./nearby-controller";
 import { create } from "./create-controller";
-import { createGymSchema, nearbyGymSchema } from "@/docs/swagger/schemas";
+import { 
+  createGymSchema, 
+  nearbyGymSchema, 
+  searchGymSchema 
+} from "@/docs/swagger/schemas";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
-  app.get("/gyms/search", search);
+  app.get("/gyms/search", searchGymSchema, search);
   app.get("/gyms/nearby", nearbyGymSchema, nearby);
 
   app.post("/gyms", { onRequest: [verifyUserRole("ADMIN")], ...createGymSchema }, create);
