de06280f811c559bebc73854dd249a40217585a5
RafaelMotaAlvess <leafardzn@gmail.com>
2024-01-10 20:29:27 -0300

    docs: created `nearby-gym-schema`

--- src/docs/swagger/schemas/gyms/index.ts
+++ src/docs/swagger/schemas/gyms/index.ts
@@ -1,1 +1,2 @@
 export * from './create-gym-schema'
+export * from './nearby-gym-schema'
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -4,13 +4,13 @@ import { FastifyInstance } from "fastify";
 import { search } from "./search-controller";
 import { nearby } from "./nearby-controller";
 import { create } from "./create-controller";
-import { createGymSchema } from "@/docs/swagger/schemas";
+import { createGymSchema, nearbyGymSchema } from "@/docs/swagger/schemas";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.get("/gyms/search", search);
-  app.get("/gyms/nearby", nearby);
+  app.get("/gyms/nearby", nearbyGymSchema, nearby);
 
   app.post("/gyms", { onRequest: [verifyUserRole("ADMIN")], ...createGymSchema }, create);
 }
