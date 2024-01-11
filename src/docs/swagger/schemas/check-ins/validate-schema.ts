b093fecdeea142913e7d07abe2c3e8f32ddef3dd
RafaelMotaAlvess <leafardzn@gmail.com>
2024-01-10 21:26:40 -0300

    docs: created `validate-schema`

---
+++ src/docs/swagger/schemas/check-ins/validate-schema.ts
@@ -0,0 +1,29 @@
+export const validateCheckInSchema = {
+  schema: {
+    description: 'Validate a check-in',
+    tags: ['checkins routes'],
+    summary: 'Validate a check-in',
+    body: {
+      type: 'object',
+      properties: {
+        checkInId: { type: 'string' },
+      },
+      required: ['checkInId']
+    },
+
+    response: {
+      204: {
+        description: 'Successful response',
+        type: 'object',
+      },
+      500: {
+        description: 'Default response',
+        type: 'object',
+        properties: {
+          message: { type: 'string', example: "Internal server error." }
+        }
+      }
+    },
+    
+  }
+}
--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -5,7 +5,7 @@ import { create } from "./create-controller";
 import { validate } from "./validate-controller";
 import { history } from "./history-controller";
 import { metrics } from "./metrics-controller";
-import { checkInsMetricsSchema, createCheckInSchema, searchCheckinsHistory } from "@/docs/swagger/schemas";
+import { checkInsMetricsSchema, createCheckInSchema, searchCheckinsHistory, validateCheckInSchema } from "@/docs/swagger/schemas";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
@@ -14,9 +14,10 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get("/check-ins/metrics",  checkInsMetricsSchema, metrics);
 
   app.post("/gyms/:gymId/check-ins", createCheckInSchema, create);
+
   app.patch(
     "/check-ins/:checkInId/validate",
-    { onRequest: [verifyUserRole("ADMIN")]  },
+    { onRequest: [verifyUserRole("ADMIN")], ...validateCheckInSchema  },
     validate,
   );
 }
